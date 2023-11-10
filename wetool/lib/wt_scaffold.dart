6cd6d88120c445e7e6858554c4752aa32f9eab91
myron6520 <100897740+myron6520@users.noreply.github.com>
2023-11-10 17:29:17 +0800

    goon

---
+++ wetool/lib/wt_scaffold.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class WTScaffold extends StatelessWidget {
+  final Widget? body;
+  final Color backgroundColor;
+  final Color appBarBackgroundColor;
+  final String title;
+  final SystemUiOverlayStyle? systemOverlayStyle;
+  final List<Widget>? actions;
+  final Widget? footer;
+  final Color footerBackgroundColor;
+  final Widget? leading;
+  const WTScaffold({
+    super.key,
+    this.body,
+    this.backgroundColor = QMColor.COLOR_F7F9FA,
+    this.title = "",
+    this.systemOverlayStyle,
+    this.actions,
+    this.appBarBackgroundColor = QMColor.COLOR_F7F9FA,
+    this.footer,
+    this.footerBackgroundColor = Colors.white,
+    this.leading,
+  });
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: backgroundColor,
+        appBar: QMAppBar(
+          title: title,
+          leading: leading,
+          systemOverlayStyle: systemOverlayStyle ??
+              SystemUiOverlayStyle.dark.copyWith(
+                systemNavigationBarColor: backgroundColor,
+              ),
+          backgroundColor: appBarBackgroundColor,
+          actions: actions,
+        ),
+        body: [
+          (body ?? Container()).expanded,
+          (footer != null).toWidget(() => footer!.toSafe().applyBackground(
+                padding: EdgeInsets.symmetric(horizontal: 16.s, vertical: 8.s),
+                color: footerBackgroundColor,
+              ))
+        ].toColumn(),
+      );
+}
